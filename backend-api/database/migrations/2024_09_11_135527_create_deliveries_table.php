91a520e99452912ff7f9fa0c62e501adcc90ad56
ruban <rubanict@gmail.com>
2024-09-12 20:31:44 +0530

    migrations

---
+++ backend-api/database/migrations/2024_09_11_135527_create_deliveries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeliveriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deliveries', function (Blueprint $table) {
+            $table->id();
+            $table->string('deliver_name');
+            $table->string('email')->unique();
+            $table->text('address');
+            $table->date('dil_date');
+            $table->string('dilivered_by');
+            $table->text('remark')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deliveries');
+    }
+}
---
+++ backend-api/database/migrations/2024_09_15_135357_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_num')->unique();
+            $table->foreignId('customer_id')->constrained()->onDelete('cascade');
+            $table->date('order_date');
+            $table->decimal('order_amount', 10, 2);
+            $table->text('order_description')->nullable();
+            $table->foreignId('delivery_id')->constrained()->onDelete('cascade');
+            $table->enum('payment_method', ['cod', 'card', 'onlinetransfer']);
+            $table->enum('status', ['open', 'processing', 'delivered', 'rejected', 'cancel']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
