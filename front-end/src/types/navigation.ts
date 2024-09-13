c31b51cbd045f9938028433003779479e29ab86b
ruban-parameshwaran <87270994+ruban-parameshwaran@users.noreply.github.com>
2024-09-13 10:31:48 +0530

    Delete front-end directory

--- front-end/src/types/navigation.ts
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export interface IMenuItems  {
-    id? : number;
-    name: string;
-    icon?: ReactNode;
-    path?: any;
-    children?: Array<IMenuItems>;
-}
